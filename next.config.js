d4e62f8383606c5870404c110257c3dd7b0e3d78
Mubashir Tanwar <tanwar0210@gmail.com>
2024-02-20 19:42:09 +0530

    update configs

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'aceternity.com',
+            port: '',
+            pathname: '/images/products/thumbnails/new/**',
+          },
+        ],
+      },
+    }
+  
+  
